03c9d4b0be55b588a3b86df50d9eebc42dd9cb5b
Koloina <rabenjamu@gmail.com>
2023-01-30 18:12:01 +0400

    update route angular

--- index.js
+++ index.js
@@ -976,6 +976,6 @@ app.listen(PORT,function () {
 
 app.use(express.static(__dirname+'/dist'));
 
-app.get('/*',function(req,res){
-	res.sendFile(path.join(__dirname+'/dist'));
+app.get('/app/*',function(req,res){
+	res.sendFile(path.join(__dirname+'/dist/index.html'));
 });
